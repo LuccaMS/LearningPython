0aaddebe6b0f134892e054ad7f2a6f0bc5e0b3a1
Lucca Machado da Silva <64712028+LuccaMS@users.noreply.github.com>
2021-05-24 10:38:10 -0300

    Update
    now the user can select option 1 or 2, option 1 will check if the cpf is valid and option 2 will generate a valid cpf

--- cpf.py
+++ cpf.py
@@ -1,32 +1,35 @@
-"""
-CPF = 168.995.350-09
-------------------------------------------------
-1 * 10 = 10           #    1 * 11 = 11 <-
-6 * 9  = 54           #    6 * 10 = 60
-8 * 8  = 64           #    8 *  9 = 72
-9 * 7  = 63           #    9 *  8 = 72
-9 * 6  = 54           #    9 *  7 = 63
-5 * 5  = 25           #    5 *  6 = 30
-3 * 4  = 12           #    3 *  5 = 15
-5 * 3  = 15           #    5 *  4 = 20
-0 * 2  = 0            #    0 *  3 = 0
-                      # -> 0 *  2 = 0
-         297          #             343
-11 - (297 % 11) = 11  #     11 - (343 % 11) = 9
-11 > 9 = 0            #
-Digito 1 = 0          #   Digito 2 = 9
-"""
+from random import randint
+
+print('Validador e gerador de cpf')
+print('Opção 1 - Digitar 11 números do cpf para verificar a validade')
+print('Opção 2 - Gerar um CPF aleatoriamente')
 
 while True:
-    cpffinal = input('Digite um cpf: ')
-    if not cpffinal.isnumeric():
-        print('Não é um número, digite novamente')
+    option = input('Por favor, digite a sua opção: ')
+    if not option.isnumeric():
+        print('Por favor, digite um número válido')
+    elif option != '1' and option != '2':
+        print('Opção inválida, digite novamente')
     else:
         break
 
-cpfaux = cpffinal[:-2]
+if option == '1':
+    while True:
+        cpffinal = input('Digite um cpf: ')
+        if not cpffinal.isnumeric():
+            print('Não é um número, digite novamente')
+
+        if len(cpffinal) < 11:
+            print('Por favor, digite um cpf com ao menos 11 números')
+        else:
+            cpfaux = cpffinal[:-2]
+            break
+
+if option == '2':
+    cpfaux = str(randint(100000000, 999999999))
 
 while True:
+
     valormultiplica = []
     vetorconta = []
     size = len(cpfaux)
@@ -53,10 +56,14 @@ while True:
     else:
         cpfaux = cpfaux + str(0)
 
-    if len(cpfaux) == len(cpffinal):
-        if cpffinal == cpfaux:
-            print('O cpf informado é válido ')
-            break
-        else:
-            print('O cpf informado é não é válido')
+    if len(cpfaux) == 11:
+        if option == '1':
+            if cpffinal == cpfaux:
+                print('O cpf informado é válido ')
+                break
+            else:
+                print('O cpf informado é não é válido')
+                break
+        if option == '2':
+            print(f'O Cpf gerado foi {cpfaux}')
             break
